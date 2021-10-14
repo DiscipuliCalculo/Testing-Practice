256ac0867eb6db93861fd08bd953c12349426e1a
Vitam Discipuli <discipulicalculo@gmail.com>
2021-10-13 19:05:07 -0700

    added new functions and caesar cipher test

--- functions.js
+++ functions.js
@@ -6,11 +6,66 @@ function capitalize(string) {
 module.exports = capitalize;
 
 function reverseString(string) {
-    let str = string.split("");
-    let newstr = ''
-    for (let i = str.length-1; i >= 0; i--){
-        newstr = newstr + str[i];
+    let str = ''
+    for (let i = string.length-1; i >= 0; i--){
+        str = str + string[i];
     }
-    return newstr;
+    return str;
 }
 module.exports = reverseString;
+/*
+const calculator = {
+    add: (a,b) => a + b,
+    subtract: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a, b) => a / b
+}
+module.exports = calculator;
+*/
+
+function caesar(string, shift) {
+    let str = ''
+    for (let i = 0; i < string.length; i++) {
+        //console.log(string[i]);
+        let letterCase = checkCase(string[i])
+        let newLetter = getLetter(string[i], letterCase, shift)
+        str = str +newLetter;
+    }
+    return str;
+}
+module.exports = caesar;
+
+function getLetter(string, letterCase, shift) {
+    let upperAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    let lowerAlphabet = 'abcdefghijklmnopqrstuvwxyz'
+    if (letterCase === 'upper') {
+        index = upperAlphabet.indexOf(string);
+        alphabet = upperAlphabet;
+    }
+    else if (letterCase === 'lower') {
+        index = lowerAlphabet.indexOf(string);
+        alphabet = lowerAlphabet;
+    }
+    else {
+        return string
+    }
+    index += shift;
+    if (index > 25){
+        index -= 26;
+    }
+    return alphabet[index];
+}
+
+function checkCase(string) {
+    if (/[A-Z]/.test(string)) {
+        return 'upper';
+    }
+    else if (/[a-z]/.test(string)) {
+        return 'lower'
+    }
+    else {
+        return 'NaL'
+    }
+}
+
+console.log(caesar('How are you? My name is __', 8))
